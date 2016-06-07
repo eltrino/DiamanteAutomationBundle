63ef72d7001e7101543b3174ddf6c53662e3b696
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2016-06-07 21:47:30 +0300

    refactor conditions logic

--- Rule/Condition/Expression/Eq.php
+++ Rule/Condition/Expression/Eq.php
@@ -20,8 +20,6 @@ use Diamante\AutomationBundle\Rule\Fact\AbstractFact;
 
 class Eq extends AbstractCondition
 {
-    const MODE = 'strict';
-
     /**
      * @param AbstractFact $fact
      *
@@ -29,8 +27,8 @@ class Eq extends AbstractCondition
      */
     public function isSatisfiedBy(AbstractFact $fact)
     {
-        $actualValue = $this->extractPropertyValue($fact);
+        $actualValue = $this->getActualValue($fact);
 
-        return $actualValue == $this->expectedValue;
+        return $actualValue == $this->context->getExpectedValue();
     }
 }
